a0ef61dc4e39c6ac5a3a9b8712566add8fdc8a59
unknown <Ofer@.(none)>
2010-08-04 11:10:11 +0300

    port to java, first version

---
+++ java/lucene.hebrew/src/org/apache/lucene/search/HebrewMaskPayloadSimilarity.java
@@ -0,0 +1,35 @@
+/**************************************************************************
+ *   Copyright (C) 2010 by                                                 *
+ *      Itamar Syn-Hershko <itamar at code972 dot com>                     *
+ *		Ofer Fort <oferiko at gmail dot com>							   *
+ *                                                                         *
+ *   Distributed under the GNU General Public License, Version 2.0.        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation (v2).                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ **************************************************************************/
+
+package org.apache.lucene.search;
+
+import org.apache.lucene.search.DefaultSimilarity;
+
+public class HebrewMaskPayloadSimilarity extends DefaultSimilarity
+{
+	@Override
+	public float scorePayload(int docId, String fieldName, int start, int end, byte[] payload, int offset, int length)
+	{
+		return 1;
+		//return base.ScorePayload(docId, fieldName, start, end, payload, offset, length);
+	}
+}
