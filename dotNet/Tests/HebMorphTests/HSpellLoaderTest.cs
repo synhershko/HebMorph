98f04eccee0d4dc661d269cec1164c3e125c07bf
synhershko <itamar@code972.com>
2010-09-02 20:18:10 +0300

    Adding new test suite

---
+++ dotNet/Tests/HebMorphTests/HSpellLoaderTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HebMorph.Tests
+{
+    [TestClass()]
+    public class HSpellLoaderTest
+    {
+        [TestMethod()]
+        public void Test1()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
---
+++ dotNet/Tests/HebMorphTests/RadixTest.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HebMorph.Tests
+{
+    [TestClass()]
+    public class RadixTest
+    {
+    }
+}
