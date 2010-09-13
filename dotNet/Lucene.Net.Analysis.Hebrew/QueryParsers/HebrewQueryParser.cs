e2a0eb90fb188b858332ef2f1ee50f864f090958
synhershko <itamar@code972.com>
2010-09-13 18:12:43 +0200

    Cleanup

--- dotNet/Lucene.Net.Analysis.Hebrew/QueryParsers/HebrewQueryParser.cs
+++ dotNet/Lucene.Net.Analysis.Hebrew/QueryParsers/HebrewQueryParser.cs
@@ -1,12 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-using Lucene.Net.Search;
-using Lucene.Net.Analysis;
+/***************************************************************************
+ *   Copyright (C) 2010 by                                                 *
+ *      Itamar Syn-Hershko <itamar at code972 dot com>                     *
+ *                                                                         *
+ *   Distributed under the GNU General Public License, Version 2.0.        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation (v2).                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ ***************************************************************************/
 
 namespace Lucene.Net.QueryParsers.Hebrew
 {
+    using System;
+    using Lucene.Net.Search;
+    using Lucene.Net.Analysis;
+
     public class HebrewQueryParser : QueryParser
     {
         protected static float SuffixedTermBoost = 2.0f;
