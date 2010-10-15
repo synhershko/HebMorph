6c2f8242077f323c6f0333e6dc2ba6f44acf092c
itaifrenkel <itaifrenkel@live.com>
2010-10-15 16:44:56 +0200

    Added automatic tests to ANT build

---
+++ java/hebmorph/test/hebmorph/RegressionTestSuite.java
@@ -0,0 +1,19 @@
+package hebmorph;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * A test suites that includes all hebmorph tests
+ * @author itaifrenkel
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	LemmatizerTest.class
+})
+public class RegressionTestSuite {
+
+}
+
+
