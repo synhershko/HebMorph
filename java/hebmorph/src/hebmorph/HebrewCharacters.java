3976b6c8d423163517451778a1ec2cc921dd60de
synhershko <itamar@code972.com>
2010-12-24 14:23:46 +0200

    Making code easier to read by using constants for Hebrew characters (Java, partial)

---
+++ java/hebmorph/src/hebmorph/HebrewCharacters.java
@@ -0,0 +1,62 @@
+/**************************************************************************
+ *   Copyright (C) 2010 by                                                 *
+ *      Itamar Syn-Hershko <itamar at code972 dot com>                     *
+ *                                                                         *
+ *   Distributed under the GNU General Public License, Version 2.0.        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation (v2).                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
+ **************************************************************************/
+package hebmorph;
+
+/**
+ * Hebrew language constants
+ * 
+ * @author synhershko
+ *
+ */
+public final class HebrewCharacters {
+	// Letters
+	public static char ALEPH	= '\u05D0';
+	public static char BET		= '\u05D1';
+	public static char GIMMEL 	= '\u05D2';
+	public static char DALET	= '\u05D3';
+	public static char HE		= '\u05D4';
+	public static char VAV	 	= '\u05D5';
+	public static char ZAYIN	= '\u05D6';
+	public static char HET		= '\u05D7';
+	public static char TET	 	= '\u05D8';
+	public static char YOD		= '\u05D9';
+	public static char KAF_FINAL= '\u05DA';
+	public static char KAF	 	= '\u05DB';
+	public static char LAMED	= '\u05DC';
+	public static char MEM_FINAL= '\u05DD';
+	public static char MEM	 	= '\u05DE';
+	public static char NUN_FINAL= '\u05DF';
+	public static char NUN		= '\u05E0';
+	public static char SAMEKH 	= '\u05E1';
+	public static char AYIN 	= '\u05E2';
+	public static char PE_FINAL	= '\u05E3';
+	public static char PE	 	= '\u05E4';
+	public static char TSADI 	= '\u05E5';
+	public static char TSADI_FINAL 	= '\u05E6';
+	public static char QUF	 	= '\u05E7';
+	public static char RESH 	= '\u05E8';
+	public static char SHIN 	= '\u05E9';
+	public static char TAV	 	= '\u05EA';
+	
+	// Punctuation
+	public static char GERESH 		= '\u05F3';
+	public static char GERSHAYIM 	= '\u05F4';
+}
