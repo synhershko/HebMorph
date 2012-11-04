2d3a7108ed0a3ddfb1fcf86ac1ccaa12aa7ff85a
synhershko <itamar@code972.com>
2012-11-04 17:23:44 +0200

    Updating license in file headers to match the software's license
    See http://www.code972.com/blog/2011/06/some-words-on-hebmorphs-licensing/

--- dotNet/HebMorph/HSpell/Loader.cs
+++ dotNet/HebMorph/HSpell/Loader.cs
@@ -1,23 +1,23 @@
 ﻿/***************************************************************************
- *   Copyright (C) 2010 by                                                 *
- *      Itamar Syn-Hershko <itamar at code972 dot com>                     *
- *                                                                         *
- *   Distributed under the GNU General Public License, Version 2.0.        *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation (v2).                                    *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Steet, Fifth Floor, Boston, MA  02111-1307, USA.          *
- ***************************************************************************/
+ * HebMorph - making Hebrew properly searchable
+ * 
+ *   Copyright (C) 2010-2012                                               
+ *      Itamar Syn-Hershko <itamar at code972 dot com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 
 using System;
 using System.Text;
