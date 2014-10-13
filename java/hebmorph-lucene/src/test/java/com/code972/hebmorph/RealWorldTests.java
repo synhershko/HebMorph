37e517d39a2b09c251526270ba7dc8e949b4c4ad
EgozyN <egozyn@gmail.com>
2014-10-13 13:11:53 +0300

    Fixed text file usage

--- java/hebmorph-lucene/src/test/java/com/code972/hebmorph/RealWorldTests.java
+++ java/hebmorph-lucene/src/test/java/com/code972/hebmorph/RealWorldTests.java
@@ -24,8 +24,7 @@ public class RealWorldTests extends TestBase {
 
     @Test
     public void test() throws IOException {
-//        File files = new File("./../../test-files");
-        File files = new File("./test-files");
+        File files = new File("./../../test-files");
         for(File filename : files.listFiles()) {
             testImpl(filename.getPath());
         }
