ae9bc360c42c38e9fb0dfae5aa240f1370599bd7
EgozyN <egozyn@gmail.com>
2015-06-07 12:27:45 +0300

    Removed old AddSuffixFilter

--- java/src/test/java/org/apache/lucene/analysis/hebrew/TestAddSuffixFilter.java
+++ java/src/test/java/org/apache/lucene/analysis/hebrew/TestAddSuffixFilter.java
@@ -17,7 +17,11 @@
  **************************************************************************/
 package org.apache.lucene.analysis.hebrew;
 
-import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.BaseTokenStreamTestCase;
+import org.apache.lucene.analysis.MockTokenizer;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.hebrew.TokenFilters.AddSuffixTokenFilter;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -28,32 +32,11 @@ public class TestAddSuffixFilter extends BaseTokenStreamTestCase {
         protected TokenStreamComponents createComponents(String fieldName,
                                                          Reader reader) {
             Tokenizer t = new MockTokenizer(reader, MockTokenizer.KEYWORD, false);
-            return new TokenStreamComponents(t, new AddSuffixFilter(t, '$') {
-                @Override
-                protected void handleCurrentToken() {
-                    duplicateCurrentToken();
-                    suffixCurrent();
-                }
-            });
-        }
-    };
-
-    Analyzer a2 = new Analyzer() {
-        @Override
-        protected TokenStreamComponents createComponents(String fieldName,
-                                                         Reader reader) {
-            Tokenizer t = new MockTokenizer(reader, MockTokenizer.KEYWORD, false);
-            return new TokenStreamComponents(t, new AddSuffixFilter(t, '$') {
-                @Override
-                protected void handleCurrentToken() {
-                    suffixCurrent();
-                }
-            });
+            return new TokenStreamComponents(t, new AddSuffixTokenFilter(t, '$'));
         }
     };
 
     public void testBasicTerms() throws IOException {
         assertAnalyzesTo(a, "book", new String[]{"book$", "book"});
-        assertAnalyzesTo(a2, "book", new String[]{"book$"});
     }
 }
